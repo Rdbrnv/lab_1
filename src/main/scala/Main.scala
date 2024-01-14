614ef4c814c4eea71beb3cbd1b51c6958bdc6925
Rdbrnv <ReDuser2000@yandex.ru>
2024-01-15 01:04:37 +0300

    All done

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,25 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
+import scala.util.Random
+
+object SumOfPairs extends App {
+
+  def generateRandomList(size: Int): List[Int] = {
+    List.fill(size)(Random.nextInt(100)) // Генерация списка случайных чисел до 100
   }
+
+  def sumOfPairs(list: List[Int], target: Int): Int = {
+    val distinctPairs = for {
+      i <- list
+      j <- list if i < j && (i + j) < target
+    } yield i + j
+
+    distinctPairs.sum
+  }
+
+  val randomListSize = 10
+  val randomList = generateRandomList(randomListSize)
+  val targetValue = 25
+
+  println(s"Сгенерированный список чисел: $randomList")
+  println(s"Заданное значение: $targetValue")
+  println(s"Сумма всех пар неодинаковых чисел меньше $targetValue: ${sumOfPairs(randomList, targetValue)}")
 }
